dbb46b1fdd0daab664fcb4da6a2ecedfbe78692b
unknown <672018141@student.uksw.edu>
2020-06-15 18:24:24 +0700

    Tambahin background dan Pattern 1

--- TTS_Grafkom_G_672018141/TTS_Grafkom_G_672018141.cpp
+++ TTS_Grafkom_G_672018141/TTS_Grafkom_G_672018141.cpp
@@ -4,14 +4,163 @@
 #include <GL/glut.h>
 #include <Windows.h>
 
+//Pattern 1 
+void Circle1(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(255, 255, 255);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 9 + 90.4;
+		double y = sin(1.9 * M_PI * i / ngon) * 12.9 + 13.9;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void LineCircle1(double r, int vertex) {
+	double ngon = (double)vertex;
+	glLineWidth(4);
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 9 + 90.4;
+		double y = sin(1.9 * M_PI * i / ngon) * 12.9 + 13.9;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void Circle2(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(255, 255, 255);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(1.9 * M_PI * i / ngon) * 9 + 71.6;
+		double y = sin(2 * M_PI * i / ngon) * 13.7 + 13.9;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void LineCircle2(double r, int vertex) {
+	double ngon = (double)vertex;
+	glLineWidth(4);
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(1.9 * M_PI * i / ngon) * 9 + 71.6;
+		double y = sin(2 * M_PI * i / ngon) * 13.7 + 13.9;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void Circle3(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(255, 255, 255);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(1.9 * M_PI * i / ngon) * 9.5 + 90.6;
+		double y = sin(2 * M_PI * i / ngon) * 12.5 + 40.8;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void LineCircle3(double r, int vertex) {
+	double ngon = (double)vertex;
+	glLineWidth(4);
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(1.9 * M_PI * i / ngon) * 9.5 + 90.6;
+		double y = sin(2 * M_PI * i / ngon) * 12.5 + 40.8;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void Circle4(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(255, 255, 255);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 9 + 71.6;
+		double y = sin(1.9 * M_PI * i / ngon) * 12.5 + 41.1;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void LineCircle4(double r, int vertex) {
+	double ngon = (double)vertex;
+	glLineWidth(4);
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 9 + 71.6;
+		double y = sin(1.9 * M_PI * i / ngon) * 12.5 + 41.1;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void SmallCircle1(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(255, 255, 255);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 2.8 + 80.9;
+		double y = sin(2 * M_PI * i / ngon) * 5 + 27;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void LineSmallCircle1(double r, int vertex) {
+	double ngon = (double)vertex;
+	glLineWidth(4);
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 2.8 + 80.9;
+		double y = sin(2 * M_PI * i / ngon) * 5 + 27;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
+void FadeCircle1(double r, int vertex) {
+	double ngon = (double)vertex;
+	glColor3ub(0, 0, 0);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < vertex; i++) {
+		double x = cos(2 * M_PI * i / ngon) * 8 + 80.9;
+		double y = sin(2 * M_PI * i / ngon) * 9 + 27;
+		glVertex2d(x, y);
+	}
+	glEnd();
+}
 
 void UpdateDisplay(int) {
 	glutTimerFunc(17, UpdateDisplay, 1);
 }
 
-
 void RenderProcess() {
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	glColor3ub(128, 0, 0);
+	glBegin(GL_QUADS);
+	{
+		glVertex2d(0, 0);
+		glVertex2d(0, 100);
+		glVertex2d(100, 100);
+		glVertex2d(100, 0);
+	}glEnd();
+
+	FadeCircle1(3.4, 80);
+
+	//Pattern 1
+	Circle1(3.4, 80);
+	LineCircle1(3.4, 80);
+	Circle2(3.4, 80);
+	LineCircle2(3.4, 80);
+	Circle3(3.4, 80);
+	LineCircle3(3.4, 80);
+	Circle4(3.4, 80);
+	LineCircle4(3.4, 80);
+	SmallCircle1(3.4, 80);
+	LineSmallCircle1(3.4, 80);
 }
 
 
